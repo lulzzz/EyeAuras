69aabf585b6392fa1e12b969b07a3dbf7a221c42
Xab3r <mail.xaber@gmail.com>
2019-11-19 00:56:21 +0300

    Added InitSymlinks.plus

--- InitSymlinks.cmd
+++ InitSymlinks.cmd
@@ -3,3 +3,10 @@ cd Sources
 mklink /J /D PoeShared.Core "../Submodules/PoeEye/PoeEye/PoeShared.Core"
 mklink /J /D PoeShared.Wpf "../Submodules/PoeEye/PoeEye/PoeShared.Wpf"
 mklink /J /D PoeShared.Tests "../Submodules/PoeEye/PoeEye/PoeShared.Tests"
+
+echo Initializing Optional modules
+mklink /J /D PoeShared.OpenCV "../Submodules/EyeAuras.Plus/PoeShared.OpenCV"
+mklink /J /D EyeAuras.OpenCVAuras "../Submodules/EyeAuras.Plus/EyeAuras.OpenCVAuras"
+mklink /J /D EyeAuras.Loader "../Submodules/EyeAuras.Plus/EyeAuras.Loader"
+mklink /J /D EyeAuras.Web "../Submodules/EyeAuras.Plus/EyeAuras.Web"
+
